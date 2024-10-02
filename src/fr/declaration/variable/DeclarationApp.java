81c71b2d21ffe849784b7bb64e2e2ea222382d93
Votre Nom <h.hassen.malek@gmail.com>
2024-10-02 15:00:18 +0200

    Ajout tp02

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,27 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+    public static void main (String[] args){
+        byte a = 12;
+        short b = 30625;
+        int c = 12500415;
+        long d = 3000000L;
+        float e = 1500000000000000030F;
+        double f =22000000000000000000000000000000000000000000000000000000000.5D;
+        char g = 'c';
+        boolean h = false;
+        String i = "Couou";
+        String randomString = "Voici le résultat d’un calcul : 1+5=6";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(randomString);
+    }
+}
